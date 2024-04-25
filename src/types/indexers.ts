8ad70f93f04dcd602260a2c82474cf35c96104f2
sorabot <admin@soramitsu.co.jp>
2024-04-25 15:08:31 +0000

    copy

--- src/types/indexers.ts
+++
@@ -1,8 +0,0 @@
-import { IndexerType } from '@soramitsu/soraneo-wallet-web/lib/consts';
-
-export interface Indexer {
-  name: string;
-  type: IndexerType;
-  endpoint: string;
-  online: boolean;
-}
